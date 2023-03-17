5cc0f69a084fca3b5b11af47c8463ede43981ae6
Omar Rezk <omar.rizk6@gmail.com>
2023-03-16 22:35:03 -0400

    finished bookpage styling

--- src/pages/Bookpage/Bookpage.jsx
+++ src/pages/Bookpage/Bookpage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Bookpage.scss'
 import bookImage from '../../assets/images.jpg'
+import facebookIcon from '../../assets/Icon-facebook.svg'
+import instagramIcon from '../../assets/Icon-instagram.svg'
+import twitterIcon from '../../assets/Icon-twitter.svg'
 
 const Bookpage = () => {
     return (
         <div className="bookpage">
             <div className="bookpage__box">
+                <div className="bookpage__block">
                 <img src={bookImage} alt="book-poster" className="bookpage__image"/>
                     <div className="bookpage__content">
                         <h2 className="bookpage__title">The Stand</h2>
                         <p className="bookpage__author">By: Stephen King</p>
+                    </div>
+                </div>
+                    <div className="bookpage__icons">
+                        <img src={facebookIcon} alt="facebook-icon" className="bookpage__icons-image" />
+                        <img src={instagramIcon} alt="instagram-icon" className="bookpage__icons-image" />
+                        <img src={twitterIcon} alt="twitter-icon" className="bookpage__icons-image" />
                     </div>
             </div>
             <div className="bookpage__container">
